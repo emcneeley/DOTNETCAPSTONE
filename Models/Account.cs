9a5678abf68cbcdcd0af24300665a2aa4ed8288e
Elijah McNeeley <elimcneeley@gmail.com>
2023-07-24 15:52:09 -0600

    TABLES DONE EXCEPT FOR MESSAGING

---
+++ Models/Account.cs
@@ -0,0 +1,9 @@
+namespace DOTNETCAPSTONE.Models;
+
+public class Account
+{
+  public string Id { get; set; }
+  public string Name { get; set; }
+  public string Email { get; set; }
+  public string Picture { get; set; }
+}
